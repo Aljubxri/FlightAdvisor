b96104b5e616151199c16bce8e3540f66a6ac9b7
aljubxri <aljubori337@gmail.com>
2022-01-01 16:14:14 -0500

    FlightAdvisor v1.1.3

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ import flightFinder
 
 
 class weather:
-    api_key = "-API KEY GOES HERE-"
+    api_key = "--API KEY GOES HERE--"
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
     # Uses Openweathermap API for current data, the api_key has been redacted for privacy purposes
